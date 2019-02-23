35e7113ff24178ea708f0a91eb5e8e3123e5dca5
Emmanuel Texier <etexier@gmail.com>
2019-02-22 17:21:58 -0800

    Simplify

--- src/main/kotlin/co/texier/kotlin/KotlinService.kt
+++ src/main/kotlin/co/texier/kotlin/KotlinService.kt
@@ -1,16 +1,9 @@
 package co.texier.kotlin
 
 class KotlinService {
-
-    data class Person(val name: String, val age: Int)
-
+    
     fun sayHello() {
         println("Kotlin says 'Hello World!'")
-
-        val alice: Person = Person("Alice", 29)
-        println(alice)
-        println(alice.age)
-        println(alice.name)
     }
 
 }
